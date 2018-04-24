fed6b98d90eda005aeaaa518d419e01a7478373a
Georgy Komarov <jubnzv@gmail.com>
2018-04-24 09:27:19 +0300

    Add simple codewars katas

---
+++ codewars/alphabetically-ordered.py
@@ -0,0 +1,6 @@
+#  https://www.codewars.com/kata/alphabetically-ordered
+
+#  Your task is very simple. Just write a function alphabetic(s), which takes an input string s in lowercase and returns True/False depending on whether the string is in alphabetical order or not.
+#  For example, alphabetic('kata') is False as 'a' comes after 'k', but alphabetic('ant') is True.
+
+def alphabetic(s): return sorted(s) == list(s)
