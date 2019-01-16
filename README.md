1db9c6831435d4dccfe47c088eba69148185a8e1
Georgy Komarov <jubnzv@gmail.com>
2019-01-16 07:12:59 +0300

    Add simple Rust tasks

--- README.md
+++ README.md
@@ -1,12 +1,14 @@
 # sandbox
 
 ## Books
+
 Path | Description
 --- | ---
 | **HR** | Examples and excercies from [The Haskell Road to Logic, Maths and Programming](https://www.amazon.com/Haskell-Logic-Programming-Second-Computing/dp/0954300696) book |
-| **AlgorithmDesign** | [The Algorithm Design Manual](https://www.amazon.com/Algorithm-Design-Manual-Steven-Skiena/dp/1849967202) by Steven S. Skiena|
+| **AlgorithmDesign** | [The Algorithm Design Manual](https://www.amazon.com/Algorithm-Design-Manual-Steven-Skiena/dp/1849967202) by Steven S. Skiena |
 
 ## Challenges
+
 Path|Description
 --- | ---
 | **euler** | Sources for [Project Euler](https://projecteuler.net/) problems |
@@ -16,3 +18,9 @@ Path|Description
 | **hackerrank** | Solutions for [hackerrank.com](https://www.hackerrank.com/) problems |
 | **leetcode** | Solutions for [leetcode.com](https://leetcode.com/) problems |
 | **geeksforgeeks** | Solutions for [geeksforgeeks.com](https://practice.geeksforgeeks.org) practice challenges |
+
+## etc.
+
+Path|Description
+--- | ---
+| **Rust** | Random Rust sources written by me |
---
+++ etc/Rust/dinning_philosophers/src/main.rs
@@ -0,0 +1,70 @@
+use std::sync::{Arc, Mutex};
+use std::thread;
+use std::time::Duration;
+
+struct Philosopher {
+    name: String,
+    left: usize,
+    right: usize,
+}
+
+impl Philosopher {
+    fn new(name: &str, left: usize, right: usize) -> Philosopher {
+        Philosopher {
+            name: name.to_string(),
+            left: left,
+            right: right,
+        }
+    }
+
+    fn eat(&self, table: &Table) {
+        let _left = table.forks[self.left].lock().unwrap();
+        thread::sleep(Duration::from_millis(150));
+        let _right = table.forks[self.right].lock().unwrap();
+
+        println!("{} starts eat", self.name);
+
+        thread::sleep(Duration::from_millis(1000));
+
+        println!("{} ends eat", self.name)
+    }
+}
+
+struct Table {
+    forks: Vec<Mutex<()>>,
+}
+
+fn main() {
+    let table = Arc::new(Table {
+        forks: vec![
+            Mutex::new(()),
+            Mutex::new(()),
+            Mutex::new(()),
+            Mutex::new(()),
+            Mutex::new(()),
+        ],
+    });
+
+    let philosophers = vec![
+        Philosopher::new("First", 0, 1),
+        Philosopher::new("Second", 1, 2),
+        Philosopher::new("Third", 2, 3),
+        Philosopher::new("Fourth", 3, 4),
+        Philosopher::new("Fifth", 0, 4),
+    ];
+
+    let handles: Vec<_> = philosophers
+        .into_iter()
+        .map(|p| {
+            let table = table.clone();
+
+            thread::spawn(move || {
+                p.eat(&table);
+            })
+        })
+        .collect();
+
+    for h in handles {
+        h.join().unwrap();
+    }
+}
